aa22d321ee4117d90e29a825cd8f06f83a4f494d
Taneli Tikkakoski <taneli.tikkakoski@aalto.fi>
2019-06-26 16:41:30 +0300

    Refactored the game and renamed buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import { Route, BrowserRouter as Router } from "react-router-dom";
-import Game from "./Game";
+import Layout from "./Layout";
 import About from "./About";
 
 function App() {
   return (
     <Router>
       <Header />
-      <Route exact path="/" component={Game} />
+      <Route exact path="/" component={Layout} />
       <Route exact path="/about" component={About} />
     </Router>
   );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,22 @@
-import { NEW_NUMBER, UPDATE_ANSWER } from '../actions/types'
+import { NEW_NUMBER, UPDATE_ANSWER, RESET_ANSWER } from "../actions/types";
 import { combineReducers } from "redux";
 
 export const newNumberReducer = (state = 0, action) => {
-  const randInt= (min,max) => {
-    return(
-      Math.floor(Math.random() * (max-min+1)) + min
-    )
-  }
+  const randInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
   if (action.type === NEW_NUMBER) {
-    return randInt(action.payload.min, action.payload.max) ;
+    return randInt(action.payload.min, action.payload.max);
   }
   return state;
-}
+};
 
-function resultReducer(state = 0, action) {
+function resultReducer(state = '', action) {
   if (action.type === UPDATE_ANSWER) {
     return Number(action.payload);
+  }
+  if (action.type === RESET_ANSWER) {
+    return '';
   }
   return state;
 }
